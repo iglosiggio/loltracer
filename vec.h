9d7b26dee435ee0394a2390b4da204eb2a20af6c
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2020-01-21 17:23:53 -0300

    vec: Use intel intrinsics

--- vec.h
+++ vec.h
@@ -2,6 +2,8 @@
 #define __VEC_H__
 
 #include <math.h>
+#include <smmintrin.h>
+#include <xmmintrin.h>
 #include "float.h"
 
 typedef struct v2 {
@@ -28,34 +30,39 @@ static inline v2 v2normalize(v2 v)
 { return v2scale(v, 1 / v2len(v)); }
 
 typedef struct v3 {
-	float x;
-	float y;
-	float z;
+	union {
+		__m128 vec;
+		struct {
+			float x;
+			float y;
+			float z;
+		};
+	};
 } v3;
 static inline v3 v3add(v3 a, v3 b)
-{ return (v3){ a.x + b.x, a.y + b.y, a.z + b.z }; }
+{ return (v3){.vec = _mm_add_ps(a.vec, b.vec)}; }
 static inline v3 v3sub(v3 a, v3 b)
-{ return (v3){ a.x - b.x, a.y - b.y, a.z - b.z }; }
+{ return (v3){.vec = _mm_sub_ps(a.vec, b.vec)}; }
 static inline v3 v3mul(v3 a, v3 b)
-{ return (v3){ a.x * b.x, a.y * b.y, a.z * b.z }; }
+{ return (v3){.vec = _mm_mul_ps(a.vec, b.vec)}; }
 static inline v3 v3div(v3 a, v3 b)
-{ return (v3){ a.x / b.x, a.y / b.y, a.z / b.z }; }
+{ return (v3){.vec = _mm_div_ps(a.vec, b.vec)}; }
 static inline float v3dot(v3 a, v3 b)
-{ return a.x * b.x + a.y * b.y + a.z * b.z; }
+{ return _mm_cvtss_f32(_mm_dp_ps(a.vec, b.vec, 0x71)); }
 static inline float v3len(v3 a)
-{ return sqrtf(a.x * a.x + a.y * a.y + a.z * a.z); }
+{ return _mm_cvtss_f32(_mm_sqrt_ss(_mm_dp_ps(a.vec, a.vec, 0x71))); }
 static inline v3 v3fill(float v)
 { return (v3){v, v, v}; }
 static inline v3 v3scale(v3 v, float f)
-{ return (v3){v.x * f, v.y * f, v.z * f}; }
+{ return (v3){.vec = _mm_mul_ps(v.vec, _mm_set1_ps(f))}; }
 static inline v3 v3normalize(v3 v)
 { return v3scale(v, 1 / v3len(v)); }
+static const int abs_mask = 0x7FFFFFFF;
 static inline v3 v3abs(v3 v)
-{ return (v3){fabsf(v.x), fabsf(v.y), fabsf(v.z)}; }
+{ return (v3){.vec = _mm_and_ps(v.vec, _mm_set1_ps(*(const float*)&abs_mask))}; }
 static inline v3 v3clamp(v3 v, float min, float max)
-{ return(v3){ clamp(v.x, min, max),
-              clamp(v.y, min, max),
-              clamp(v.z, min, max) }; }
+{ return(v3){.vec = _mm_max_ps(_mm_min_ps(v.vec, _mm_set1_ps(max)),
+                               _mm_set1_ps(min))}; }
 static inline v3 v3pow(v3 v, float pow)
 { return (v3){ powf(v.x, pow), powf(v.y, pow), powf(v.z, pow) }; }
 static inline v3 v3cross(v3 a, v3 b)
